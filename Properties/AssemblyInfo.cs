7f516c7642e68acb38d3d4db6b66c9a30a536f82
ayomejabi <ayomejabi2@outlook.com>
2018-03-17 17:40:36 +0000

    ano commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ebay Automation test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Ebay Automation test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5621d54c-98f4-4650-8554-77ea199af5af")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SearchForACar.feature
@@ -0,0 +1,19 @@
+﻿Feature: SearchForACar
+	In order to search for a mazda cx-5
+	As an ebay customer
+	I want to be able to search for a mazda cx-5 with 5 miles of m30 7qf
+
+@mytag
+Scenario: to search for a car using postcode
+Given I navigate to ebay website. www.ebay.co.uk
+And I hover over the text field search for anything
+When i type in Mazda cx-5
+And i hover over to advance search 
+And i click on advanced
+And i scroll down to location
+And i select the located radio button
+And i select distance from the drop down list
+And i type in postcode
+And i click on search
+Then all mazda cx-5 within distance of postcode should appear
+
